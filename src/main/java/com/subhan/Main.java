03841581d9dd3c5c8449633e78d812c96e7d1fef
subhankm <mabusubhan.k@gmail.com>
2022-01-27 00:20:22 +0530

    second commit

--- src/main/java/com/subhan/Main.java
+++ src/main/java/com/subhan/Main.java
@@ -3,5 +3,6 @@ package com.subhan;
 public class Main {
     public static void main(String[] args) {
         System.out.println("JAVA & JENKINS INTEGRATION APP");
+        System.out.println("JAVA & JENKINS INTEGRATION APP");
     }
 }
